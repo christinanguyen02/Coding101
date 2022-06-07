b28c44f55ba0b62ad6b5a1bf523518330ee384a1
Christina <christinanguyen765@utexas.edu>
2022-06-07 16:37:35 -0500

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,65 @@
 import React from "react";
 import "./projects.css";
-import cry from "../kuromicry.png";
+import cube from "../kurocube.png";
+import { Row, Col, Card, Container, Button } from "react-bootstrap";
+import background from "../blackbackground.jpeg";
+import GenZ from "../GenZ.png";
 
 export default function Projects() {
   return (
     <>
       <div className="ProjectsPage">
-        <p className="pagetext">
-          In Progress . . .&nbsp;
-          <img className="Kuro" src={cry} alt="cry"></img>
-        </p>
+        <Container fluid>
+          <Row>
+            <p className="pagetext">
+              Projects
+              <img className="Kuro" src={cube} alt="cry"></img>
+            </p>
+            <p></p>
+          </Row>
+          <Row className="projectcards">
+            <Col className="single" align="center">
+              <Card style={{ width: "25rem" }}>
+                <Card.Img variant="top" src={GenZ} />
+                <Card.Body>
+                  <Card.Title>GenZ Trading</Card.Title>
+                  <Card.Text>
+                    TradeZ is a multi-functional platform for cryptocurrency
+                    trading for generation Z.
+                  </Card.Text>
+                  <Button
+                    variant="secondary"
+                    href="https://devpost.com/software/tradez-a-genz-crypto-trading-platform"
+                    target="_blank"
+                  >
+                    See More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="single" align="center">
+              <Card style={{ width: "25rem" }}>
+                <Card.Img variant="top" src={background} />
+                <Card.Body>
+                  <Card.Title>In Progress</Card.Title>
+                  <Card.Text>...</Card.Text>
+                  {/* <Button variant="primary">Go somewhere</Button> */}
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="single" align="center">
+              <Card style={{ width: "25rem" }}>
+                <Card.Img variant="top" src={background} />
+                <Card.Body>
+                  <Card.Title>In Progress</Card.Title>
+                  <Card.Text>...</Card.Text>
+                  {/* <Button variant="primary">Go somewhere</Button> */}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
-      ;
     </>
   );
 }
